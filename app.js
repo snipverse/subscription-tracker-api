e52059f1b99e67e9e72a6700d51f867e8a6ecd1b
snipverse <sachinpal9324@gmail.com>
2025-12-22 09:28:52 +0530

    initialize routes, models, and error middleware

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import { PORT } from "./config/env.js"
+import connectToDatabase  from "./database/mongodb.js";
+
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
+import errorMiddleware from "./middleware/error.middleware.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Subscription tracker API')
+});
+
+app.listen(PORT, async() =>{
+    console.log(`Subscription tracker API in running on http://localhost:${PORT}`);
+    await connectToDatabase();
+});
+
+export default app;
